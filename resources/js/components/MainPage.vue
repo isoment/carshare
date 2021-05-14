06cb330c9a36fbfbad41dd6f6b3c4551675c9a0e
twill <twill1@gmx.com>
2021-05-14 17:49:25 -0500

    Add vue slick carousel and create a car make slider

--- resources/js/components/MainPage.vue
+++ resources/js/components/MainPage.vue
@@ -70,20 +70,105 @@
                         <img src="/img/info-icon-1.svg" class="h-10 w-10 mb-3">
                         <h3 class="font-black text-bold font-boldnosans mb-2">Wide selection of cars</h3>
                         <p class="text-gray-700 font-light mb-2 text-center sm:text-left">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores illo quia, eaque velit ullam sit magni ad. Aspernatur odit excepturi.</p>
-                        <a href="#" class="font-bold text-purple-600">Learn More</a>
+                        <a href="#" class="font-bold text-purple-600 hover:text-purple-500">Learn More</a>
                     </div>
                     <div class="mb-12 lg:mb-0 flex flex-col items-center sm:items-start">
                         <img src="/img/info-icon-2.svg" class="h-10 w-10 mb-3 -ml-2">
                         <h3 class="font-black text-bold font-boldnosans mb-2">Cars near you</h3>
                         <p class="text-gray-700 font-light mb-2 text-center sm:text-left">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores illo quia, eaque velit ullam sit magni ad. Aspernatur odit excepturi.</p>
-                        <a href="#" class="font-bold text-purple-600">Learn More</a>
+                        <a href="#" class="font-bold text-purple-600 hover:text-purple-500 transition-all duration-200">
+                            Learn More
+                        </a>
                     </div>
                     <div class="mb-12 lg:mb-0 flex flex-col items-center sm:items-start">
                         <img src="/img/info-icon-3.svg" class="h-10 w-10 mb-3">
                         <h3 class="font-black text-bold font-boldnosans mb-2">Strict safety standards</h3>
                         <p class="text-gray-700 font-light mb-2 text-center sm:text-left">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores illo quia, eaque velit ullam sit magni ad. Aspernatur odit excepturi.</p>
-                        <a href="#" class="font-bold text-purple-600">Learn More</a>
+                        <a href="#" class="font-bold text-purple-600 hover:text-purple-500 transition-all duration-200">
+                            Learn More
+                        </a>
                     </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Car make slider -->
+        <section class="my-24">
+            <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 mt-14">
+                <h3 class="font-black text-bold font-boldnosans mb-4 mx-8 text-xl">Browse by make</h3>
+                <div>
+                    <vue-slick-carousel v-bind="carouselSettings" class="mx-6">
+                        <template #prevArrow="arrowOption">
+                            <i class="fas fa-chevron-left text-lg custom-arrow">
+                                {{ arrowOption.currentSlide }}/{{ arrowOption.slideCount }}
+                            </i>
+                        </template>
+                        <template #nextArrow="arrowOption">
+                            <i class="fas fa-chevron-left text-lg custom-arrow">
+                                {{ arrowOption.currentSlide }}/{{ arrowOption.slideCount }}
+                            </i>
+                        </template>
+                        <!-- Card -->
+                        <div>
+                            <div class="max-w-sm rounded-lg overflow-hidden shadow-md mx-2 my-3 hover:shadow-lg
+                                        transition-all transform hover:-translate-y-1 duration-300">
+                                <img class="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains">
+                                <div class="px-6 py-3 text-center">
+                                    <div class="font-bold text-lg mb-2">Toyota</div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Card -->
+                        <div>
+                            <div class="max-w-sm rounded-lg overflow-hidden shadow-md mx-2 my-3 hover:shadow-lg
+                                        transition-all transform hover:-translate-y-1 duration-300">
+                                <img class="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains">
+                                <div class="px-6 py-3 text-center">
+                                    <div class="font-bold text-lg mb-2">Toyota</div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Card -->
+                        <div>
+                            <div class="max-w-sm rounded-lg overflow-hidden shadow-md mx-2 my-3 hover:shadow-lg
+                                        transition-all transform hover:-translate-y-1 duration-300">
+                                <img class="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains">
+                                <div class="px-6 py-3 text-center">
+                                    <div class="font-bold text-lg mb-2">Toyota</div>
+                                </div>
+                            </div>
+                        </div>      
+                        <!-- Card -->
+                        <div>
+                            <div class="max-w-sm rounded-lg overflow-hidden shadow-md mx-2 my-3 hover:shadow-lg
+                                        transition-all transform hover:-translate-y-1 duration-300">
+                                <img class="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains">
+                                <div class="px-6 py-3 text-center">
+                                    <div class="font-bold text-lg mb-2">Toyota</div>
+                                </div>
+                            </div>
+                        </div>      
+                        <!-- Card -->
+                        <div>
+                            <div class="max-w-sm rounded-lg overflow-hidden shadow-md mx-2 my-3 hover:shadow-lg
+                                        transition-all transform hover:-translate-y-1 duration-300">
+                                <img class="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains">
+                                <div class="px-6 py-3 text-center">
+                                    <div class="font-bold text-lg mb-2">Toyota</div>
+                                </div>
+                            </div>
+                        </div>      
+                        <!-- Card -->
+                        <div>
+                            <div class="max-w-sm rounded-lg overflow-hidden shadow-md mx-2 my-3 hover:shadow-lg
+                                        transition-all transform hover:-translate-y-1 duration-300">
+                                <img class="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains">
+                                <div class="px-6 py-3 text-center">
+                                    <div class="font-bold text-lg mb-2">Toyota</div>
+                                </div>
+                            </div>
+                        </div>                                                                                                      
+                    </vue-slick-carousel>
                 </div>
             </div>
         </section>
@@ -92,9 +177,74 @@
 </template>
 
 <script>
+    import VueSlickCarousel from 'vue-slick-carousel';
+    import 'vue-slick-carousel/dist/vue-slick-carousel.css';
+    import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css';
+
     export default {
+        name: 'MainPage',
+
+        components: {
+            VueSlickCarousel
+        },
+
+        data() {
+            return {
+                carouselSettings: {
+                    "dots": false,
+                    "infinite": true,
+                    "initialSlide": 2,
+                    "slidesToShow": 5,
+                    "slidesToScroll": 1,
+                    "swipeToSlide": true,
+                    "responsive": [
+                        {
+                            "breakpoint": 1200,
+                            "settings": {
+                                "arrows": true,
+                                "dots": false,
+                                "slidesToShow": 4,
+                                "slidesToScroll": 1,
+                                "infinite": true,
+                            }
+                        },
+                        {
+                            "breakpoint": 900,
+                            "settings": {
+                                "arrows": true,
+                                "dots": false,
+                                "slidesToShow": 3,
+                                "slidesToScroll": 1,
+                                "infinite": true,
+                            }
+                        },
+                        {
+                            "breakpoint": 700,
+                            "settings": {
+                                "arrows": false,
+                                "dots": false,
+                                "slidesToShow": 2,
+                                "slidesToScroll": 1,
+                                "infinite": true,
+                            }
+                        }
+                    ]
+                }
+            }
+        },
+
         computed: {},
 
         methods: {}
     }
 </script>
+
+<style scoped>
+    .slick-next::before {
+        color: #9f7aea;
+    }
+
+    .slick-prev::before{
+        color: #9f7aea;
+    }
+</style>
