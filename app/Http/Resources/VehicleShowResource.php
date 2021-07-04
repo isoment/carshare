72b0a7e6879a8b93e44f246cfb03f4077c9f9514
twill <twill1@gmx.com>
2021-07-03 23:23:00 -0500

    Update VehicleShowResource to filter JSON

--- app/Http/Resources/VehicleShowResource.php
+++ app/Http/Resources/VehicleShowResource.php
@@ -14,6 +14,24 @@ class VehicleShowResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'vehicle_model' => $this['vehicle_model']['model'],
+            'vehicle_make' => $this['vehicle_model']['vehicle_make']['make'],
+            'year' => $this['year'],
+            'price' => $this['price_day'],
+            'description' => $this['description'],
+            'doors' => $this['doors'],
+            'seats' => $this['seats'],
+            'vehicle_images' => $this['vehicle_images'],
+            'host_name' => $this['host']['name'],
+            'top_host' => $this['host']['top_host'],
+            'member_since' => $this['host']['created_at'],
+            'host_total_trips' => $this['host_total_trips'],
+            'host_rating' => $this['host_rating'],
+            'vehicle_rating' => $this['vehicle_rating'],
+            'vehicle_review_count' => $this['vehicle_review_count'],
+            'vehicle_trip_count' => $this['vehicle_trip_count'],
+            'vehicle_reviews' => $this['vehicle_reviews']
+        ];
     }
 }
