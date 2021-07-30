206317dc4c227e125069f1db4b9d6ed6fdcc7ca6
Isoment <twill1@gmx.com>
2021-07-30 15:30:34 -0500

    Add tests for user review api endpoints

--- tests/Feature/ReviewTest.php
+++ tests/Feature/ReviewTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use App\Models\Vehicle;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
@@ -9,6 +10,44 @@ use Tests\TestCase;
 class ReviewTest extends TestCase
 {
     use RefreshDatabase;
+
+    /**
+     *  The format we expect to see for an index of reviews
+     * 
+     *  @return array
+     */
+    public function reviewIndexJsonStructure()
+    {
+        return [
+            'data' => [
+                '*' => [
+                    'rating',
+                    'content',
+                    'created_at',
+                    'updated_at',
+                    'reviewer_name',
+                    'reviewer_avatar'
+                ]
+            ],
+            'links' => [
+                'first', 'last', 'prev', 'next'
+            ],
+            'meta' => [
+                'current_page',
+                'from',
+                'last_page',
+                'links' => [
+                    '*' => [
+                        'url', 'label', 'active'
+                    ]
+                ],
+                'path',
+                'per_page',
+                'to',
+                'total'
+            ]
+        ];
+    }
 
     /**
      *  @test
@@ -23,34 +62,44 @@ class ReviewTest extends TestCase
         $response = $this->json('GET', '/api/reviews-vehicle/' . $randomVehicle->id);
 
         $response->assertStatus(200)
-            ->assertJsonStructure([
-                'data' => [
-                    '*' => [
-                        'rating',
-                        'content',
-                        'created_at',
-                        'updated_at',
-                        'reviewer_name',
-                        'reviewer_avatar'
-                    ]
-                ],
-                'links' => [
-                    'first', 'last', 'prev', 'next'
-                ],
-                'meta' => [
-                    'current_page',
-                    'from',
-                    'last_page',
-                    'links' => [
-                        '*' => [
-                            'url', 'label', 'active'
-                        ]
-                    ],
-                    'path',
-                    'per_page',
-                    'to',
-                    'total'
-                ]
-            ]);
+            ->assertJsonStructure(
+                $this->reviewIndexJsonStructure()
+            );
+    }
+
+    /**
+     *  @test
+     *  A paginated index is returned of a users reviews from hosts
+     */
+    public function the_users_reviews_from_hosts_are_displayed_as_json()
+    {
+        $this->createSmallDatabase();
+
+        $randomUser = User::all()->random();
+
+        $response = $this->json('GET', '/api/reviews-from-hosts/' . $randomUser->id);
+
+        $response->assertStatus(200)
+            ->assertJsonStructure(
+                $this->reviewIndexJsonStructure()
+            );
+    }
+
+    /**
+     *  @test
+     *  A paginated index is returned of a users reviews from renters
+     */
+    public function the_users_reviews_from_renters_are_displayed_as_json()
+    {
+        $this->createSmallDatabase();
+
+        $randomUser = User::all()->random();
+
+        $response = $this->json('GET', '/api/reviews-from-renters/' . $randomUser->id);
+
+        $response->assertStatus(200)
+            ->assertJsonStructure(
+                $this->reviewIndexJsonStructure()
+            );
     }
 }
