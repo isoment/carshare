ca8ddef9be9bf15f171858d7c589c28d02e8f9ec
twill <twill1@gmx.com>
2021-08-15 18:06:32 -0500

    Test profile update api endpoint

--- tests/Feature/ProfileTest.php
+++ tests/Feature/ProfileTest.php
@@ -6,7 +6,6 @@ use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Http\UploadedFile;
-use Laravel\Sanctum\Sanctum;
 use Illuminate\Support\Str;
 use Tests\TestCase;
 
@@ -21,16 +20,6 @@ class ProfileTest extends TestCase
     public function unauthorized_users_cannot_access_the_update_avatar_api_endpoint()
     {
         $this->json('POST', '/api/dashboard/update-avatar')
-            ->assertStatus(401);
-    }
-
-    /**
-     *  @test
-     *  Unauthorized users cannot access the update profile api endpoint.
-     */
-    public function unauthorized_users_cannot_access_the_update_profile_api_endpoint()
-    {
-        $this->json('PUT', '/api/dashboard/update-profile')
             ->assertStatus(401);
     }
 
@@ -97,5 +86,55 @@ class ProfileTest extends TestCase
         ])->assertStatus(200);
 
         $this->assertTrue(Str::contains($user->profile->image, '.jpeg'));
+    }
+
+    /**
+     *  @test
+     *  Unauthorized users cannot access the update profile api endpoint.
+     */
+    public function unauthorized_users_cannot_access_the_update_profile_api_endpoint()
+    {
+        $this->json('PUT', '/api/dashboard/update-profile')
+            ->assertStatus(401);
+    }
+
+    /**
+     *  @test
+     *  An authorized user receives a 204 status when no data is provided to the
+     *  profile update api endpoint
+     */
+    public function authorized_users_receive_204_status_when_no_data_is_passed()
+    {
+        $user = User::factory()->create();
+
+        $this->actingAs($user);
+
+        $this->json('PUT', '/api/dashboard/update-profile', [])
+            ->assertStatus(204);
+    }
+
+    /**
+     *  @test
+     *  An authorized user can update their profile
+     */
+    public function an_authorized_user_can_update_their_profile()
+    {
+        $user = User::factory()->create();
+
+        $this->actingAs($user);
+
+        $dataArray = [
+            'location' => 'Toronto, CA',
+            'languages' => 'English, French',
+            'work' => 'Software Engineer',
+            'school' => 'Harvard',
+            'about' => 'This is my profile, there are many profiles but this is mine.'
+        ];
+
+        $this->json('PUT', '/api/dashboard/update-profile', $dataArray)->assertStatus(204);
+
+        $withUserId = array_merge($dataArray, ['user_id' => $user->id]);
+
+        $this->assertDatabaseHas('profiles', $withUserId);
     }
 }
