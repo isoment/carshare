a4595f413c4f274c2e233e9569675e76f34dcf6f
twill <twill1@gmx.com>
2021-09-17 23:54:01 -0500

    Add processing and spinner when making payment, update confirmation page component

--- resources/js/cart/Confirmation.vue
+++ resources/js/cart/Confirmation.vue
@@ -7,7 +7,27 @@
         </div>
 
         <div v-else>
-
+            <div class="md:mt-8 flex items-center justify-center px-5 pb-10 pt-16">
+                <div class="w-full mx-auto rounded-lg bg-white shadow-lg border p-5 text-gray-700" style="max-width: 600px">
+                    <div class="w-full pt-1 pb-5">
+                        <div class="bg-green-500 text-white overflow-hidden rounded-full w-20 h-20 -mt-16 
+                                    mx-auto shadow-lg flex justify-center items-center">
+                            <svg class="w-12 h-12" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4"></path></svg>
+                        </div>
+                    </div>
+                    <div class="text-center">
+                        <h3 class="text-2xl font-boldnosans font-bold">Thank you for booking with us!</h3>
+                        <h6 class="mt-4">We have sent you an email with the details of your booking. Our hosts look forward to providing a great experience for you</h6>
+                    </div>
+                    <div class="flex items-center justify-center my-3">
+                        <svg class="w-24 h-24 text-green-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
+                    </div>
+                    <div class="text-center">
+                        <h3 class="text-2xl font-boldnosans font-bold text-center">Confirmation:</h3>
+                        <h3 class="text-green-400 text-2xl font-boldnosans font-bold">{{ pid }}</h3>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
--- resources/js/cart/Payment.vue
+++ resources/js/cart/Payment.vue
@@ -49,13 +49,22 @@
                         </div>
                     </div>
                     <div class="mt-8">
-                        <button class="block w-full max-w-xs mx-auto bg-purple-500 hover:bg-purple-400 focus:bg-purple-700 
-                                      text-white px-3 py-3 transition-all duration-200"
+                        <button class="block w-full max-w-xs mx-auto text-white px-3 py-3 transition-all duration-200"
+                                :disabled="processingPayment"
+                                :class="{ 
+                                    'bg-gray-400': processingPayment, 
+                                    'bg-purple-500 hover:bg-purple-400': !processingPayment 
+                                }"
                                 @click="processPayment">
                             <div class="flex justify-center items-center">
-                                <svg class="w-4 h-4 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"></path>
-                                </svg> 
+                                <div v-if="processingPayment">
+                                    <i class="fas fa-spinner fa-spin"></i>
+                                </div>
+                                <div v-else>
+                                    <svg class="w-4 h-4 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"></path>
+                                    </svg> 
+                                </div>
                                 <span class="ml-2">Pay now</span>
                             </div>
                         </button>
