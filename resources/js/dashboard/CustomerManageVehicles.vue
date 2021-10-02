4752ad53544047ad615a2d65f5de8b303b7d6ac0
twill <twill1@gmx.com>
2021-10-02 00:19:48 -0500

    Bug fix filter dropdown menu on CustomerManageVehicles component

--- resources/js/dashboard/CustomerManageVehicles.vue
+++ resources/js/dashboard/CustomerManageVehicles.vue
@@ -5,22 +5,28 @@
                 <div class="rounded border-2 border-purple-400 bg-white px-6 py-3">
                     <h3 class="text-lg font-boldnosans font-bold">
                         <span>Manage Vehicles</span>
-                        <i class="fas fa-cogs ml-1 cursor-pointer"
-                           ref="dropmenu"
-                           @click="toggleFilterMenu"></i>
+                        <button>
+                            <i class="fas fa-cogs ml-1 cursor-pointer"
+                               ref="menubutton"
+                               @click="toggleFilterMenu"></i>
+                        </button>
                     </h3>
                 </div>
             </div>
             <!-- Filter dropdown -->
-            <div class="absolute py-4 px-2 bg-white border border-grey-50 rounded-md shadow-md 
-                        top-8 filtermenu"
-                 v-if="menuState">
-                TEST
-            </div>
+            <transition name="fade">
+                <div class="absolute py-4 px-2 bg-white border border-grey-50 rounded-md shadow-md 
+                            top-8 filtermenu"
+                    ref="filtermenu"
+                    v-if="menuState">
+                    Test menu content
+                </div>
+            </transition>
+
         </div>
 
         <div class="py-8">
-            TEST
+            Vehicle index will go here
         </div>
     </div>
 </template>
@@ -39,7 +45,7 @@
 
                 // Remove or add an event listener
                 const closeListener = (e) => {
-                    if (_this.catchOutsideClick(e, _this.$refs.dropmenu)) {
+                    if (_this.catchOutsideClick(e, _this.$refs.menubutton, _this.$refs.filtermenu)) {
                         window.removeEventListener('click', closeListener)
                         _this.menuState = false;
                     }
@@ -51,12 +57,16 @@
             },
 
             // Determine if a click is outside the menu
-            catchOutsideClick(event, dropdown) {
-                if (dropdown == event.target) {
+            catchOutsideClick(event, button, menu) {
+                if (button == event.target || menu == event.target) {
                     return false;
                 }
 
-                if (this.menuState && dropdown != event.target) {
+                if (this.menuState && button != event.target) {
+                    return true;
+                }
+
+                if (this.menuState && menu != event.target) {
                     return true;
                 }
             }
