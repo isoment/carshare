a802f0caef13f846073d2b39d2c3b66eff9e4037
twill <twill1@gmx.com>
2021-10-02 11:18:14 -0500

    Refactor filter dropdown in CustomerManageVehicles to seperate component

--- resources/js/dashboard/CustomerManageVehicles.vue
+++ resources/js/dashboard/CustomerManageVehicles.vue
@@ -5,24 +5,10 @@
                 <div class="rounded border-2 border-purple-400 bg-white px-6 py-3">
                     <h3 class="text-lg font-boldnosans font-bold">
                         <span>Manage Vehicles</span>
-                        <button>
-                            <i class="fas fa-cogs ml-1 cursor-pointer"
-                               ref="menubutton"
-                               @click="toggleFilterMenu"></i>
-                        </button>
+                        <customer-manage-vehicles-filter></customer-manage-vehicles-filter>
                     </h3>
                 </div>
             </div>
-            <!-- Filter dropdown -->
-            <transition name="fade">
-                <div class="absolute py-4 px-2 bg-white border border-grey-50 rounded-md shadow-md 
-                            top-8 filtermenu"
-                    ref="filtermenu"
-                    v-if="menuState">
-                    Test menu content
-                </div>
-            </transition>
-
         </div>
 
         <div class="py-8">
@@ -32,50 +18,11 @@
 </template>
 
 <script>
+    import CustomerManageVehiclesFilter from './dropdowns/CustomerManageVehiclesFilter';
+
     export default {
-        data() {
-            return {
-                menuState: false
-            }
-        },
-
-        methods: {
-            toggleFilterMenu() {
-                var _this = this;
-
-                // Remove or add an event listener
-                const closeListener = (e) => {
-                    if (_this.catchOutsideClick(e, _this.$refs.menubutton, _this.$refs.filtermenu)) {
-                        window.removeEventListener('click', closeListener)
-                        _this.menuState = false;
-                    }
-                }
-
-                window.addEventListener('click', closeListener);
-
-                this.menuState = !this.menuState;
-            },
-
-            // Determine if a click is outside the menu
-            catchOutsideClick(event, button, menu) {
-                if (button == event.target || menu == event.target) {
-                    return false;
-                }
-
-                if (this.menuState && button != event.target) {
-                    return true;
-                }
-
-                if (this.menuState && menu != event.target) {
-                    return true;
-                }
-            }
+        components: {
+            CustomerManageVehiclesFilter
         }
     }
 </script>
-
-<style scoped>
-    .filtermenu {
-        width: 16rem;
-    }
-</style>
---
+++ resources/js/dashboard/dropdowns/CustomerManageVehiclesFilter.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="inline">
+        <!-- Toggle Button -->
+        <button>
+            <i class="fas fa-cogs ml-1 cursor-pointer"
+            ref="menubutton"
+            @click="toggleFilterMenu"></i>
+        </button>
+
+        <!-- Filter Dropdown -->
+        <transition name="fade">
+            <div class="absolute py-4 px-2 bg-white border border-grey-50 rounded-md shadow-md 
+                        left-0 top-16 filtermenu text-sm font-normal"
+                ref="filtermenu"
+                v-if="menuState">
+                Test menu content
+            </div>
+        </transition>
+    </div>
+</template>
+
+<script>
+    import dropdownToggle from './../../shared/mixins/dropdownToggle';
+
+    export default {
+        mixins: [dropdownToggle]
+    }
+</script>
+
+<style scoped>
+    .filtermenu {
+        width: 16rem;
+    }
+</style>
