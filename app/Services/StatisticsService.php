e62fbb237e657ecd7ab65285373e78d47ae68d4f
twill <twill1@gmx.com>
2022-02-02 18:58:14 -0600

    Add basic stats and bookings count by month in the statistics api

--- app/Services/StatisticsService.php
+++ app/Services/StatisticsService.php
@@ -4,12 +4,80 @@ declare(strict_types=1);
 
 namespace App\Services;
 
+use App\Models\Booking;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\DB;
+
 class StatisticsService
 {
     public function showRenterStats()
     {
-        return [
+        $user = current_user();
+        $usersOrders = $user->orders;
+        $bookingsByMonth = $this->bookingsByMonth($usersOrders);
 
+        return [
+            'basic' => $this->basicStats($user, $usersOrders),
+            'bookingCountByMonth' => [
+                'month' => array_keys($bookingsByMonth),
+                'count' => array_values($bookingsByMonth)
+            ]
         ];
+    }
+
+    /**
+     *  @param User $user
+     *  @param Collection $orders
+     *  @return array
+     */
+    public function basicStats(User $user, Collection $orders) : array
+    {
+        return [
+            'totalSpent' => $orders->sum('total'),
+            'bookingCount' => $user->getBookings()->count(),
+            'cancelCount' => $user->getCancellationsAsRenter()->count(),
+            'orderCount' => $orders->count(),
+            'orderAverage' => $orders->average('total')
+        ];
+    }
+
+    /**
+     *  @param Collection $orders
+     */
+    public function bookingsByMonth(Collection $orders) : array
+    {
+        $countByMonth = Booking::whereIn('order_id', $orders->pluck('id'))
+            ->whereBetween('from', [Carbon::now()->subMonths(3), Carbon::now()->addMonths(4)])
+            ->select(
+                DB::raw("DATE_FORMAT(`from`, '%Y-%m') month"),
+                DB::raw("count('month') as booking_count")
+            )
+            ->groupBy('month')
+            ->orderBy('month')
+            ->get();
+
+        $flatArr = [];
+
+        foreach ($countByMonth as $m) {
+            $flatArray[ Carbon::parse($m['month'])->format('M')] = $m['booking_count'];
+        }
+
+        return $flatArray;
+    }
+
+    /**
+     *  @param Collection $orders
+     */
+    public function bookingsByMonthWithNulls(Collection $orders)
+    {
+        return DB::select('
+            SELECT `from`,
+                   `to`,
+                   price_total
+            FROM bookings
+            LEFT
+        ');
     }
 }
--- resources/js/dashboard/CustomerStatistics.vue
+++ resources/js/dashboard/CustomerStatistics.vue
@@ -1,24 +1,34 @@
 <template>
     <div>
         <main-navigation></main-navigation>
-        <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 mb-6">
-        <div class="mt-8">
-            <div class="flex items-center justify-end mb-4">
-                <span class="mr-2 font-bold font-boldnosans text-gray-700">As Host:</span>
-                <label for="booking-stat-toggle" class="flex items-center cursor-pointer">
-                    <div class="relative">
-                        <input type="checkbox" 
-                            id="booking-stat-toggle" 
-                            class="sr-only"
-                            v-model="hostMode">
-                        <div class="booking-stat-toggle-bg block bg-gray-200 w-14 h-7 rounded-full"></div>
-                        <div class="booking-stat-toggle-dot absolute left-2 top-1 bg-white w-5 h-5 rounded-full transition"></div>
-                    </div>
-                </label>
-            </div>
-            <host-statistics v-if="hostMode"></host-statistics>
-            <renter-statistics v-else></renter-statistics>
+        <div v-if="!isLoggedIn">
+            <error :message="'Not Authorized'"></error>
         </div>
+        <div>
+            <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 mb-6">
+            <div class="mt-8">
+                <div class="flex items-center justify-end mb-4">
+                    <span class="mr-2 font-bold font-boldnosans text-gray-700">As Host:</span>
+                    <label for="booking-stat-toggle" class="flex items-center cursor-pointer">
+                        <div class="relative">
+                            <input type="checkbox" 
+                                id="booking-stat-toggle" 
+                                class="sr-only"
+                                v-model="hostMode">
+                            <div class="booking-stat-toggle-bg block bg-gray-200 w-14 h-7 rounded-full"></div>
+                            <div class="booking-stat-toggle-dot absolute left-2 top-1 bg-white w-5 h-5 rounded-full transition"></div>
+                        </div>
+                    </label>
+                </div>
+                <div v-if="loading" class="text-center mt-8">
+                    <i class="fas fa-spinner fa-spin text-purple-500 text-4xl"></i>
+                </div>
+                <div v-else>
+                    <host-statistics v-if="hostMode"></host-statistics>
+                    <renter-statistics v-else :stats="stats"></renter-statistics>
+                </div>
+            </div>
+            </div>
         </div>
     </div>
 </template>
@@ -26,6 +36,7 @@
 <script>
     import RenterStatistics from './statistic-components/RenterStatistics.vue';
     import HostStatistics from './statistic-components/HostStatistics.vue';
+    import { mapState } from 'vuex';
 
     export default {
         components: {
@@ -33,11 +44,37 @@
             HostStatistics
         },
 
+        computed: {
+            ...mapState({
+                isLoggedIn: "isLoggedIn",
+                user: "user"
+            }),
+        },
+
         data() {
             return {
-                hostMode: false
+                hostMode: false,
+                stats: null,
+                loading: false
             }
-        }
+        },
+
+        methods: {
+            async fetchRenterStats() {
+                try {
+                    this.loading = true;
+                    let response = await axios.get('/api/dashboard/renter-stats');
+                    this.stats = response.data;
+                } catch (error) {
+                    console.log(error);
+                }
+                this.loading = false;
+            }
+        },
+
+        created() {
+            this.fetchRenterStats();
+        },
     }
 </script>
 
