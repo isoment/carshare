4fb28376f3c65a5f49730671b6648180f139e5de
twill <twill1@gmx.com>
2022-05-28 14:01:25 -0500

    Create a DemoInfo component the triggers a modal for some application info

---
+++ resources/js/shared/components/DemoInfo.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        <button class="focus:outline-none demo-info-button mr-2"
+                :class="[iconColorStyle, hoverColorStyle]"
+                @click="showDemoInfoModal = true">
+            <svg class="w-7 h-7" fill="none" 
+                 stroke="currentColor" 
+                 viewBox="0 0 24 24" 
+                 xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+            </svg>
+        </button>
+
+        <simple-modal v-model="showDemoInfoModal" @close="closeDemoInfoModal">
+            <div>Hello :)</div>
+        </simple-modal>
+    </div>
+</template>
+
+<script>
+    import SimpleModal from '../../shared/components/SimpleModal.vue';
+
+    export default {
+        components: {
+            SimpleModal
+        },
+
+        props: {
+            color: String,
+            hoverColor: String
+        },
+
+        computed: {
+            iconColorStyle() {
+                return this.color ? `text-${this.color}` : 'text-gray-400';
+            },
+
+            hoverColorStyle() {
+                return this.hoverColor ? `hover:text-${this.hoverColor}` : 'hover:text-white'
+            }
+        },
+
+        data() {
+            return {
+                showDemoInfoModal: false
+            }
+        },
+
+        methods: {
+            closeDemoInfoModal() {
+                this.showDemoInfoModal = false;
+            }
+        }
+    }
+</script>
+
+<style>
+    .demo-info-button {
+        margin-top: 5px;
+    }
+</style>
--- resources/js/shared/components/NavigationBarMain.vue
+++ resources/js/shared/components/NavigationBarMain.vue
@@ -59,8 +59,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"
-                        v-if="isLoggedIn">
+                    <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                         <!-- <button class="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-purple-400">
                             <span class="sr-only">View notifications</span>
                             <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
@@ -69,8 +68,9 @@
                                     stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
                             </svg>
                         </button> -->
-                        <shopping-cart-link></shopping-cart-link>
-                        <profile-dropdown></profile-dropdown>
+                        <demo-info></demo-info>
+                        <shopping-cart-link v-if="isLoggedIn"></shopping-cart-link>
+                        <profile-dropdown v-if="isLoggedIn"></profile-dropdown>
                     </div>
                 </div>
             </div>
@@ -113,11 +113,13 @@
     import { mapState } from 'vuex';
     import ProfileDropdown from './ProfileDropdown.vue';
     import ShoppingCartLink from './ShoppingCartLink.vue';
+    import DemoInfo from './DemoInfo.vue';
 
     export default {
         components: {
             ProfileDropdown,
-            ShoppingCartLink
+            ShoppingCartLink,
+            DemoInfo
         },
 
         data() {
